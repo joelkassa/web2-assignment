8f1a42b555e1e16219ef8f65be99b32449a6298a
joelkassa <joelkassa159@gmail.com>
2026-04-25 10:34:45 -0700

    server updated

--- server.js
+++ server.js
@@ -14,9 +14,35 @@ const writeMovies = (movies) => {
   fs.writeFileSync(DATA_FILE, JSON.stringify(movies, null, 2));
 };
 
+const sendJSON = (res, statusCode, data) => {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(data));
+};
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: 'Movie Review API is running' }));
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/movies' && method === 'GET') {
+    const movies = readMovies();
+    sendJSON(res, 200, movies);
+  }
+
+  else if (url.startsWith('/movies/') && method === 'GET') {
+    const id = parseInt(url.split('/')[2]);
+    const movies = readMovies();
+    const movie = movies.find(m => m.id === id);
+
+    if (movie) {
+      sendJSON(res, 200, movie);
+    } else {
+      sendJSON(res, 404, { message: 'Movie not found' });
+    }
+  }
+
+  else {
+    sendJSON(res, 404, { message: 'Route not found' });
+  }
 });
 
 server.listen(PORT, () => {
